0401c2683ee9c6e20ac975d1e886070bd8ba2a71
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-28 20:50:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,9 +6,9 @@ class Author
     @posts = [] 
   end 
   
-  def add_post(post) 
-    @posts << post 
-    post.author = self 
-  end 
+  # def add_post(post) 
+  #   @posts << post 
+  #   post.author = self 
+  # end 
   
 end 
